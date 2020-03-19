75f9da032b8e8f3653a8b49814a9ecd8c8eff0f7
flofriday <flohacksfriday@gmail.com>
2020-03-19 01:20:17 +0100

    Add build script

---
+++ build_package.sh
@@ -0,0 +1,4 @@
+# If you want to build a package with pyinstaller this is the command.
+# However on macOS you must have python insatlled via homebrew and not form
+# python.org
+pyinstaller Resize\ Tool.py --onedir --windowed -s
